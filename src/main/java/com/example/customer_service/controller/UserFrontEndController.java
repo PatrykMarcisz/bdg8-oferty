a12c4d1346b2f61a6e01d65e29ea5dc75a85ecee
MikiKru <michal_kruczkowski@o2.pl>
2020-02-23 15:39:51 +0100

    Spring Thyme

--- src/main/java/com/example/customer_service/controller/UserFrontEndController.java
+++ src/main/java/com/example/customer_service/controller/UserFrontEndController.java
@@ -1,7 +1,14 @@
 package com.example.customer_service.controller;
 
+import com.example.customer_service.model.User;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import javax.validation.Valid;
 
 @Controller
 public class UserFrontEndController {
@@ -10,9 +17,20 @@ public class UserFrontEndController {
     public String index(){
         return "index";     // nazwa widoku .html do wyświetlenia
     }
-    @GetMapping("/registration")
-    public String registration(){
+    @GetMapping("/registration")                        // <- to wywołuje formularz rejestracji
+    public String registration(Model model){            // model komunikuje back-end z szablonem Thymeleaf
+        model.addAttribute("user", new User());       // przekazujemy do Thymelaf resolvera obiekt user
         return "registration";
+    }
+    @PostMapping("/registration")                       // <- to przekazywane są dane rejestracji
+    public String registration(@ModelAttribute @Valid User user, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            // poprawianie błędów
+            return "registration";
+        }
+        // zapis do bazy
+        System.out.println(user);
+        return "index";
     }
 
 }
