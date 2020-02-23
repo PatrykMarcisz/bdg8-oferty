8c74fd3f975312cd11379941a5d42f075bf98053
MikiKru <michal_kruczkowski@o2.pl>
2020-02-23 12:43:04 +0100

    Spring REST

--- src/main/java/com/example/customer_service/repository/UserRepository.java
+++ src/main/java/com/example/customer_service/repository/UserRepository.java
@@ -6,6 +6,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PathVariable;
 
 import java.util.List;
@@ -22,6 +23,7 @@ public interface UserRepository extends JpaRepository<User, Long>{
     )
     List<Object[]> findAllEmailAndRoleName();
     // aktywuj wszystkich użytkowników adnotacją Query
+    @Transactional
     @Modifying                                                      // executeUpdate
     @Query(value = "UPDATE user SET status = :status", nativeQuery = true)
     void changeStatusToUsers(@Param("status") Boolean status);
