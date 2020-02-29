ff4dd8cb00d37cce46c3dd7f96942c6aa8ab2a6e
MikiKru <michal_kruczkowski@o2.pl>
2020-02-29 14:30:56 +0100

    spring v8 - security config

--- src/main/java/com/example/customer_service/configuration/SecurityConfiguration.java
+++ src/main/java/com/example/customer_service/configuration/SecurityConfiguration.java
@@ -1,9 +1,13 @@
 package com.example.customer_service.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity      // klasa implementująca metody configure dla http, jdbc, oauth2
@@ -28,6 +32,19 @@ public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
                     .logout()
                     .logoutUrl("/logout")
                     .logoutSuccessUrl("/");
-
+    }
+    @Autowired
+    DataSource dataSource;
+    @Autowired
+    PasswdEncoder passwdEncoder;
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+            .jdbcAuthentication()
+                .usersByUsernameQuery("SELECT u.email, u.password, u.status FROM user u WHERE u.email = ?") // wyszukanie użytkownika po adresie email
+                .authoritiesByUsernameQuery(
+                        "SELECT u.email, r.role_name FROM user u JOIN user_role ur ON (u.user_id = ur.user_id) " +
+                                "JOIN role r ON (r.role_id = ur.role_id) WHERE u.email = ?")
+                .dataSource(dataSource)
+                .passwordEncoder(passwdEncoder.getPasswordEncoder());       // matoda zwracająca algorytm BCryptPasswordEncoder
     }
 }
