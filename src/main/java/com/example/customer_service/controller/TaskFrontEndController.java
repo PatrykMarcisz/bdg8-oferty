6f8bcd23ec6619b8138f6b865d4fd4ee70ac80db
MikiKru <michal_kruczkowski@o2.pl>
2020-02-29 13:20:32 +0100

    spring v7 - task controller

--- src/main/java/com/example/customer_service/controller/TaskFrontEndController.java
+++ src/main/java/com/example/customer_service/controller/TaskFrontEndController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import javax.validation.Valid;
@@ -37,5 +38,15 @@ public class TaskFrontEndController {
 //        }
         taskService.addTask(task.getContent(), task.getPrice(), new HashSet<>(), null);
         return "redirect:/";
+    }
+    @GetMapping("/tasks&{taskId}")
+    public String addTaskToBasket(
+            @PathVariable("taskId") Long taskId,
+            Model model
+    ){
+        model.addAttribute("info", "zlecenie zostało przyjęte");
+        model.addAttribute("tasks", taskService.getAllTasksOrderByPublicationDateDesc());
+        // ???
+        return "index";
     }
 }
